5abfffe738bbeb49ab9cdb9eaa7f9ab106869dc9
Teoh Kheng Hong Neurogine <tkhenghong@gmail.com>
2020-03-04 14:45:09 +0800

    Init commit

---
+++ src/main/java/com/in28minutes/springboot/tutorial/basics/example/springboottutorialbasicsaop/SpringBootTutorialBasicsAopApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.tutorial.basics.example.springboottutorialbasicsaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorialBasicsAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorialBasicsAopApplication.class, args);
+	}
+
+}
